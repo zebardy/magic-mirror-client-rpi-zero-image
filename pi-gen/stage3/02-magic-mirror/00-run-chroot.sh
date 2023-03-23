26e9f10f9c49ab8dda597ea54a19294d7eecce14
Aaron Moses <Aaron Moses>
2023-03-23 00:49:01 +0000

    automatically login as pi user and start a chromium kiosk connected to the magic-mirror server

---
+++ pi-gen/stage3/02-magic-mirror/00-run-chroot.sh
@@ -0,0 +1,44 @@
+#cd ~
+#git clone https://github.com/MichMich/MagicMirror
+#cd MagicMirror/
+#npm run install-mm
+#cp config/config.js.sample config/config.js
+cat <<EOT > /home/pi/bin/start-chromium.sh
+#!/bin/sh
+
+set -e
+
+CHROMIUM_TEMP=/tmp/chromium
+rm -Rf /home/pi/.config/chromium/
+rm -Rf \$CHROMIUM_TEMP
+mkdir -p \$CHROMIUM_TEMP
+
+chromium-browser \
+        --disable \
+        --disable-translate \
+        --disable-infobars \
+        --disable-suggestions-service \
+        --disable-save-password-bubble \
+        --disk-cache-dir=\$CHROMIUM_TEMP/cache/ \
+        --user-data-dir=\$CHROMIUM_TEMP/user_data/ \
+        --start-maximized \
+        --kiosk http://$host:$port &
+EOT
+
+cat <<EOT > /home/pi/.config/lxsession/LXDE-pi/autostart
+@lxpanel --profile LXDE-pi
+@pcmanfm --desktop --profile LXDE-pi
+@xscreensaver -no-splash
+@point-rpi
+@sh /home/pi/bin/start-chromium.sh
+EOT
+
+mkdir -pv /etc/systemd/system/getty@tty1.service.d
+
+cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin pi --noclear %I \$TERM
+EOF
+
+echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && exec startx" >> /home/pi/.profile
--- pi-gen/stage3/02-magic-mirror/00-run.sh
+++
@@ -1,5 +0,0 @@
-cd ~
-git clone https://github.com/MichMich/MagicMirror
-cd MagicMirror/
-npm run install-mm
-cp config/config.js.sample config/config.js
